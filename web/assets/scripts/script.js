27aee54dbd0c61b2d535c2b95061e7a329139937
justinshin6 <justinshin@brown.edu>
2024-02-14 15:48:55 -0500

    transfer over navbar script for mobile

---
+++ web/assets/scripts/script.js
@@ -0,0 +1,8 @@
+// Navbar script
+function openNav() {
+    document.getElementById("navholder-mobile").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("navholder-mobile").style.height = "0%";
+}
